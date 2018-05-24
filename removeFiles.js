ba7b75b273dece8f8947185daf876f3a046202bd
Danverde <gre12014@byui.edu>
2018-05-24 10:49:41 -0600

    Much shorter!

--- removeFiles.js
+++ removeFiles.js
@@ -1,7 +1,6 @@
 /*eslint-env node, es6*/
 
-/* Deletes all created files that aren't to be used by the user.
-In debug mode, the files are not deleted. */
+/* Deletes all created files that aren't to be used by the user. */
 
 /* Put dependencies here */
 const rimraf = require('rimraf');
@@ -9,57 +8,90 @@ const asyncLib = require('async');
 
 module.exports = (course, stepCallback) => {
 
-    /* If not wanting to keep files */
-    if (!course.settings.keepFiles) {
+    /*  function deleteOriginalZip(callback) {
+        rimraf(course.info.originalZipPath, err => {
+            if (err) {
+                course.error(err);
+            } else {
+                course.message('Original course zip was successfully removed');
+            }
+            callback();
+        });
+    }
 
-        asyncLib.waterfall([
-            (callback) => {
-                rimraf(course.info.originalZipPath, err => {
-                    if (err) {
-                        course.error(err);
-                    } else {
-                        course.message('Original course zip was successfully removed');
-                    }
-                    callback();
-                });
+    function deleteUnzipped(callback) {
+        rimraf(course.info.unzippedPath, err => {
+            if (err) {
+                course.error(err);
+            } else {
+                course.message('Unzipped original course was successfully removed');
             }
-            ,
-            (callback) => {
-                rimraf(course.info.unzippedPath, err => {
-                    if (err) {
-                        course.error(err);
-                    } else {
-                        course.message('Unzipped original course was successfully removed');
-                    }
-                    callback();
-                });
+            callback();
+        });
+    }
+
+    function deleteProcessed(callback) {
+        rimraf(course.info.processedPath, err => {
+            if (err) {
+                course.error(err);
+            } else {
+                course.message('Unzipped altered course successfully removed');
             }
-            ,
-            (callback) => {
-                rimraf(course.info.processedPath, err => {
-                    if (err) {
-                        course.error(err);
-                    } else {
-                        course.message('Unzipped altered course successfully removed');
-                    }
-                    callback();
-                });
+            callback();
+        });
+    }
+
+    function deleteUploadZip(callback) {
+        rimraf(course.info.uploadZipPath, err => {
+            if (err) {
+                course.error(err);
+            } else {
+                course.message('Generated zip successfully removed');
             }
-            ,
-            (callback) => {
-                rimraf(course.info.uploadZipPath, err => {
-                    if (err) {
-                        course.error(err);
-                    } else {
-                        course.message('Generated zip successfully removed');
-                    }
-                    callback();
-                });
+            callback();
+        });
+    } */
+
+    function deleteFolder(folder, callback) {
+        if (folder == undefined || folder === 'Unspecified') {
+            course.message(`Unable to delete ${folder}`);
+            callback();
+            return;
+        }
+
+        rimraf(folder, err => {
+            if (err) {
+                course.error(err);
+            } else {
+                course.message(`${folder.split('\\')[folder.split('\\').length - 1]}Directory successfully removed`);
             }
-        ], () => {
+            callback();
+        });
+    }
+
+
+    /* If not wanting to keep files */
+    if (course.settings.keepFiles) {
+        stepCallback(null, course);
+    } else {
+        var foldersToRemove = [
+            course.info.originalZipPath,
+            course.info.unzippedPath,
+            course.info.processedPath,
+            course.info.uploadZipPath
+        ];
+        
+        asyncLib.eachSeries(foldersToRemove, deleteFolder, () => {
             stepCallback(null, course);
         });
-    } else {
-        stepCallback(null, course);
+
+        /* asyncLib.waterfall([
+            //deleteOriginalZip,
+            deleteProcessed,
+            deleteUnzipped,
+            deleteUploadZip
+        ], () => {
+            stepCallback(null, course);
+        }); */
     }
 };
